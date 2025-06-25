abd941fc860247339b34634f0bcc9c8ce76f8a11
A7442 <parkjuna1121@naver.com>
2025-06-26 03:40:48 +0900

    feat : jumpAction스크립트 수정

--- Assets/02_Scripts/Enemy/JumpAction.cs
+++ Assets/02_Scripts/Enemy/JumpAction.cs
@@ -3,6 +3,7 @@ using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
+using Random = System.Random;
 
 [Serializable, GeneratePropertyBag]
 [NodeDescription(name: "Jump", story: "[Self] Jump [Room] [jumpCount]", category: "Action", id: "43fbb0a0c7718811425f745e0952091a")]
@@ -15,15 +16,26 @@ public partial class JumpAction : Action
     private float _jumpForce = 5.0f;
     private float _horizontalForce;//수치 조정 필요
     private float _jumpTime = 3.0f;
+    private Rigidbody2D _rigidbody2D;
 
     protected override Status OnStart()
     {
+        if (Self.Value == null)
+        {
+            return Status.Failure;
+        }
+        if (_rigidbody2D == null)
+        {
+            _rigidbody2D = Self.Value.GetComponent<Rigidbody2D>();
+        }
+        StartJump();
         return Status.Running;
     }
 
     private void StartJump()
     {
-        
+        var direction = 1;//여기 개선
+        _rigidbody2D.AddForce(new Vector2(_horizontalForce*direction, _jumpForce),ForceMode2D.Impulse);
     }
 
     protected override Status OnUpdate()
