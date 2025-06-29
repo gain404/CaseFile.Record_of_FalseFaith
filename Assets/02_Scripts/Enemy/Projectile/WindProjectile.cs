d78f1d47ea50dacf212c0eb9a0a503ed580e231b
A7442 <parkjuna1121@naver.com>
2025-06-30 01:08:19 +0900

    feat : WindProjectile스크립트 완성

--- Assets/02_Scripts/Enemy/Projectile/WindProjectile.cs
+++ Assets/02_Scripts/Enemy/Projectile/WindProjectile.cs
@@ -1,6 +1,39 @@
+using System;
 using UnityEngine;
 
 public class WindProjectile : MonoBehaviour
 {
+    [Header("Projectile Settings")]
+    [SerializeField] private int damage = 1;
+    [SerializeField] private LayerMask hitLayerMask;
     
+    private Rigidbody2D _rigidbody2D;
+    private PoolManager _poolManager;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _poolManager = PoolManager.Instance;
+    }
+
+    private void OnEnable()
+    {
+        if (transform.rotation.y > 90)
+        {
+            _rigidbody2D.linearVelocityX = 1.0f;
+        }
+        else
+        {
+            _rigidbody2D.linearVelocityX = -1.0f;
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == hitLayerMask)
+        {
+            //플레이어에 데미지
+            _poolManager.Return(PoolKey.WindProjectile, this.gameObject);
+        }
+    }
 }
