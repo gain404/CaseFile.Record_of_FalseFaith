ea926562fa853506872dcac0fdedd85089b37136
A7442 <parkjuna1121@naver.com>
2025-07-03 22:57:07 +0900

    feat : HairProjectile 스크립트 작성

--- Assets/02_Scripts/Enemy/Projectile/1stage/HairProjectile.cs
+++ Assets/02_Scripts/Enemy/Projectile/1stage/HairProjectile.cs
@@ -3,61 +3,38 @@ using DG.Tweening;
 
 public class HairProjectile : MonoBehaviour
 {
+    
+
     [Header("Projectile Settings")]
     [SerializeField] private int damage = 1;
-    [SerializeField] private float returnTime = 5f;
     [SerializeField] private LayerMask hitLayerMask;
     
     private PoolManager _poolManager;
-    private GameObject _player;
     private Tween _moveTween;
+    private Animator _animator;
+    private readonly int _hairAttack = Animator.StringToHash("HairAttack");
 
     private void Awake()
     {
         _poolManager = PoolManager.Instance;
-        _player = GameObject.FindWithTag("Player");
+        _animator = gameObject.GetComponent<Animator>();
     }
 
     private void OnEnable()
     {
-        DOVirtual.DelayedCall(returnTime, ReturnProjectile).SetTarget(this);
-        RepeatFollow();
-    }
-
-    void RepeatFollow()
-    {
-        DOVirtual.DelayedCall(0.5f, () =>
-        {
-            if (_player == null) return;
-
-            Vector3 targetPos = _player.transform.position;
-            _moveTween?.Kill();
-            _moveTween = transform.DOMove(targetPos, 0.5f).SetEase(Ease.Linear);
-
-            RepeatFollow();
-        }).SetTarget(this);
+        DOVirtual.DelayedCall(0.5f, () => _animator.SetTrigger(_hairAttack));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (((1 << collision.gameObject.layer) & hitLayerMask) != 0)
         {
-            if (collision.gameObject.TryGetComponent<IDamagable>(out var damagable))
-            {
-                damagable.TakeDamage(damage);
-            }
-            ReturnProjectile();
+            //플레이어 데미지
         }
     }
-
-    private void OnDisable()
-    {
-        _moveTween?.Kill();
-        DOTween.Kill(this);
-    }
-
+    
     private void ReturnProjectile()
     {
-        _poolManager.Return(PoolKey.WindProjectile, gameObject);
+        _poolManager.Return(PoolKey.HairProjectile, gameObject);
     }
 }
