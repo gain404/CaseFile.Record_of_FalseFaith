5d7c37897944fe63c7aad0c592769c5e2cb37dad
songdh001 <155062686+songdh001@users.noreply.github.com>
2025-07-08 23:16:50 +0900

    feat : 달리기 상태에 진입할 때 카메라가 더 멀리서 찍는 기능 추가

---
+++ Assets/02_Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController Instance { get; private set; }
+
+    [SerializeField] private CinemachineCamera virtualCamera;
+
+    private float defaultSize = 10f; //기본 카메라
+    private float runningSize = 15f; //달릴 때 카메라
+    private float zoomSpeed = 5f; //얼마나 빨리 변할지
+
+    private Coroutine zoomCoroutine;
+
+    private void Awake()
+    {
+        Instance = this;
+        if (virtualCamera == null)
+        {
+            virtualCamera = FindFirstObjectByType<CinemachineCamera>();
+        }
+    }
+
+
+    public void ZoomOutForRunning()
+    {
+        if (zoomCoroutine != null) StopCoroutine(zoomCoroutine);
+        zoomCoroutine = StartCoroutine(ZoomTo(runningSize));
+    }
+
+    public void ZoomInToDefault()
+    {
+        if (zoomCoroutine != null) StopCoroutine(zoomCoroutine);
+        zoomCoroutine = StartCoroutine(ZoomTo(defaultSize));
+    }
+
+    private IEnumerator ZoomTo(float targetSize)
+    {
+        float currentSize = virtualCamera.Lens.OrthographicSize;
+
+        while (Mathf.Abs(currentSize - targetSize) > 0.01f)
+        {
+            currentSize = Mathf.Lerp(currentSize, targetSize, Time.deltaTime * zoomSpeed);
+            virtualCamera.Lens.OrthographicSize = currentSize;
+            yield return null;
+        }
+
+        virtualCamera.Lens.OrthographicSize = targetSize;
+    }
+}
--- Assets/02_Scripts/Player/FSM/MoveState/PlayerRunState.cs
+++ Assets/02_Scripts/Player/FSM/MoveState/PlayerRunState.cs
@@ -1,4 +1,4 @@
-
+﻿
 
 public class PlayerRunState : PlayerMoveState
 {
@@ -11,12 +11,14 @@ public class PlayerRunState : PlayerMoveState
         base.Enter();
         stateMachine.MovementSpeedModifier = moveData.RunSpeedModifier;
         StartAnimation(stateMachine.Player.PlayerAnimationData.RunParameterHash);
+        CameraController.Instance.ZoomOutForRunning();
     }
 
     public override void Exit()
     {
         base.Exit();
         EndAnimation(stateMachine.Player.PlayerAnimationData.RunParameterHash);
+        CameraController.Instance.ZoomInToDefault();
     }
     
 }
