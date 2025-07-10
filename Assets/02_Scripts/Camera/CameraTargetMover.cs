d8a6674209a9970178eb0297689ff31dd7f5d5e6
songdh001 <155062686+songdh001@users.noreply.github.com>
2025-07-10 16:23:04 +0900

    feat : 달리기 상태일 때 카메라가 살짝 위로 올라가도록 추가

---
+++ Assets/02_Scripts/Camera/CameraTargetMover.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class CameraTargetMover : MonoBehaviour
+{
+    /// <summary>
+    /// 카메라 타겟의 위치를 달릴 때 수정해주기 위한 스크립트입니다.
+    /// </summary>
+
+    private Vector3 originalLocalPos;
+
+    private void Awake()
+    {
+        originalLocalPos = transform.localPosition;
+    }
+
+
+    public void MoveToOffset(Vector3 offset, float duration)
+    {
+        transform.DOLocalMove(originalLocalPos + offset, duration);
+    }
+
+    public void ResetPosition(float duration)
+    {
+        transform.DOLocalMove(originalLocalPos, duration);
+    }
+}
--- Assets/02_Scripts/Player/FSM/MoveState/PlayerRunState.cs
+++ Assets/02_Scripts/Player/FSM/MoveState/PlayerRunState.cs
@@ -1,7 +1,7 @@
 ﻿
-
 public class PlayerRunState : PlayerMoveState
 {
+    private CameraTargetMover cameraTargetMover;
     public PlayerRunState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
     {
     }
@@ -9,6 +9,12 @@ public class PlayerRunState : PlayerMoveState
     public override void Enter()
     {
         base.Enter();
+        if (cameraTargetMover == null)
+        {
+            cameraTargetMover = stateMachine.Player.transform.Find("CameraTarget").GetComponent<CameraTargetMover>();
+        }
+
+        cameraTargetMover.MoveToOffset(new UnityEngine.Vector3(0f, 0.7f, 0f), 0.5f); // 예시: 위로 0.7만큼 0.5초 동안
         stateMachine.MovementSpeedModifier = moveData.RunSpeedModifier;
         StartAnimation(stateMachine.Player.PlayerAnimationData.RunParameterHash);
         CameraController.Instance.ZoomOutForRunning();
@@ -18,6 +24,7 @@ public class PlayerRunState : PlayerMoveState
     public override void Exit()
     {
         base.Exit();
+        cameraTargetMover.ResetPosition(0.3f);
         EndAnimation(stateMachine.Player.PlayerAnimationData.RunParameterHash);
         CameraController.Instance.ZoomInToDefault();
         CameraController.Instance.isRunning = false;
