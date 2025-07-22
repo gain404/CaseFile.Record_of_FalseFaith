8a6e6b429f1f41f04aa6236460cd8c1614c0234d
A7442 <parkjuna1121@naver.com>
2025-07-22 18:07:48 +0900

    feat : 원하는 페이지 이동 구현

--- Assets/02_Scripts/UI/UIList/UIBook.cs
+++ Assets/02_Scripts/UI/UIList/UIBook.cs
@@ -11,22 +11,40 @@ public class UIBook : MonoBehaviour
     [SerializeField] private GameObject forwardButton;
     [SerializeField] private GameObject backButton;
 
-    private int _index;//저장해놓기
+    [SerializeField]private int _index;//저장해놓기
     private bool _isRotating;
+    private int _targetIndex;
+    private bool _isFastRotating;
 
-    private void Awake()
+    private void Start()
+    {
+        InitState();
+    }
+
+    private void OnEnable()
+    {
+        //전 페이지 기억
+    }
+
+    private void InitState()
     {
         _index = -1;
+        _isRotating = false;
+        foreach (Transform page in pages)
+        {
+            page.transform.rotation = Quaternion.identity;
+        }
+        pages[0].SetAsFirstSibling();
         backButton.SetActive(false);
     }
 
     public void RotateForward()
     {
-        if (_isRotating) return;
+        if (_isRotating || _isFastRotating) return;
         _index++;
         NextButtonAction();
         pages[_index].SetAsLastSibling();
-        RotatePage(180f, true);
+        RotatePage(180f, pageSpeed,true);
     }
 
     private void NextButtonAction()
@@ -44,10 +62,10 @@ public class UIBook : MonoBehaviour
 
     public void RotateBack()
     {
-        if (_isRotating) return;
+        if (_isRotating || _isFastRotating) return;
         BackButtonAction();
         pages[_index].SetAsLastSibling();
-        RotatePage(0f, false);
+        RotatePage(0f, pageSpeed,false);
     }
 
     private void BackButtonAction()
@@ -62,15 +80,51 @@ public class UIBook : MonoBehaviour
             backButton.SetActive(false);
         }
     }
-
-    private void RotatePage(float angleY, bool forward)
+    
+    //어떤 페이지의 왼쪽이 몇page인지 index 넣기
+    public void ChoosePage(int index)
     {
-        pages[_index].DOLocalRotate(new Vector3(0, angleY, 0), pageSpeed)
+        if (_isFastRotating || _isRotating) return;
+        _targetIndex = index;
+        FastRotate();
+    }
+
+    private void FastRotate()
+    {
+        if(_index == _targetIndex - 1)
+        {
+            _isFastRotating = false;
+            return;
+        }
+
+        _isFastRotating = true;
+        if(_index < _targetIndex -1)
+        {
+            _index++;
+            NextButtonAction();
+            pages[_index].SetAsLastSibling();
+            RotatePage(180f, 0.2f,true, FastRotate);
+        }
+        else
+        {
+            BackButtonAction();
+            pages[_index].SetAsLastSibling();
+            RotatePage(0f, 0.2f,false, FastRotate);
+        }
+    }
+
+    private void RotatePage(float angleY, float speed, bool isForward, System.Action onComplete = null)
+    {
+        _isRotating = true;
+        pages[_index].DOLocalRotate(new Vector3(0, angleY, 0), speed)
             .SetEase(Ease.InOutSine)
             .OnComplete(() =>
             {
-                if (!forward)
-                    _index--;
+                if (!isForward) _index--;
+                _isRotating = false;
+                onComplete?.Invoke(); // 콜백 함수 실행
             });
     }
 }
+
+//다음페이지랑 전 페이지 뜨는거 fast에 추가
