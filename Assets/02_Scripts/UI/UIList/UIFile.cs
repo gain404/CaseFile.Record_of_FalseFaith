044afd497669dab5aa87f1768a23cccd15273a33
A7442 <parkjuna1121@naver.com>
2025-07-23 23:33:01 +0900

    feat : ui우선도 추가

--- Assets/02_Scripts/UI/UIList/UIFile.cs
+++ Assets/02_Scripts/UI/UIList/UIFile.cs
@@ -1,6 +1,55 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class UIFile : MonoBehaviour
 {
+    public static List<List<bool>> SurveyActive { get; private set; }
+
+    [SerializeField] private GameObject file;
+    [SerializeField] private List<Button> chapterButton;
     
+    [SerializeField] private List<Button> buttons;
+    //설명 데이터 리스트로 받아오기
+    [SerializeField] private List<string> explainNames;
+    [SerializeField] private List<string> explains;
+    [SerializeField] private List<TextMeshProUGUI> buttonText;
+    [SerializeField] private TextMeshProUGUI explainNameText;
+    [SerializeField] private TextMeshProUGUI explainText;
+    [SerializeField] private UISurvey uiSurvey;
+    
+    private void Start()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            int capturedIndex = i;
+            buttons[i].onClick.AddListener(() => OnExplain(capturedIndex));
+            buttonText[i] = buttons[i].GetComponent<TextMeshProUGUI>();
+        }
+        for (int j = 0; j < chapterButton.Count; j++)
+        {
+            int chapter = j;
+            chapterButton[j].onClick.AddListener(() => SetButtons(chapter));
+        }
+    }
+
+    private void SetButtons(int chapter)
+    {
+        file.transform.SetAsLastSibling();
+        buttons[chapter].gameObject.transform.SetAsLastSibling();
+        //데이터 넣기
+    }
+    
+    public void OnActiveUi(int chapter, int index)
+    {
+        SurveyActive[chapter][index] = true;
+        buttonText[index].text = explainNames[index];
+    }
+    
+    private void OnExplain(int index)
+    {
+        explainText.text = explains[index];
+        explainNameText.text = explainNames[index];
+    }
 }
--- Assets/02_Scripts/UI/UIList/UISurvey.cs
+++ Assets/02_Scripts/UI/UIList/UISurvey.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,19 +7,23 @@ public class UISurvey : MonoBehaviour
 {
     [SerializeField] private Button bookButton;
     [SerializeField] private GameObject uiFile;
+
+    private UIFile _uiFile;
     
     private void Start()
     {
-        bookButton.onClick.AddListener(OnBook);
+        bookButton.onClick.AddListener(ActiveBook);
         uiFile.SetActive(false);
+        _uiFile = uiFile.GetComponent<UIFile>();
     }
 
+    //조사 시트 버튼을 active시켜주는 메써드
     public void BookButtonActive()
     {
         bookButton.gameObject.SetActive(true);
     }
 
-    private void OnBook()
+    private void ActiveBook()
     {
         if (!uiFile.activeSelf)
         {
@@ -28,5 +33,12 @@ public class UISurvey : MonoBehaviour
         {
             uiFile.SetActive(false);
         }
+    }
+    
+
+    //이부에서 정보를 수집했을 때 호출되는 메써드
+    public void GetSurvey(int chapter, int index)
+    {
+        _uiFile.OnActiveUi(chapter, index);
     }
 }
