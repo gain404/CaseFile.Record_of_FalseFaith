fff836810bda5c394648ed55a066281efbb65724
A7442 <parkjuna1121@naver.com>
2025-07-24 22:36:08 +0900

    feat : survey수정

--- Assets/02_Scripts/UI/UIList/UIFile.cs
+++ Assets/02_Scripts/UI/UIList/UIFile.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class UIFile : MonoBehaviour
 {
@@ -10,22 +11,27 @@ public class UIFile : MonoBehaviour
     [SerializeField] private GameObject file;
     [SerializeField] private List<Button> chapterButton;
     
-    [SerializeField] private List<Button> buttons;
+    [SerializeField] private List<Button> listButtons;
     //설명 데이터 리스트로 받아오기
     [SerializeField] private List<string> explainNames;
     [SerializeField] private List<string> explains;
-    [SerializeField] private List<TextMeshProUGUI> buttonText;
     [SerializeField] private TextMeshProUGUI explainNameText;
     [SerializeField] private TextMeshProUGUI explainText;
     [SerializeField] private UISurvey uiSurvey;
-    
+
+    private List<TextMeshProUGUI> _buttonText = new List<TextMeshProUGUI>();
     private void Start()
     {
-        for (int i = 0; i < buttons.Count; i++)
+        for (int i = 0; i < listButtons.Count; i++)
         {
             int capturedIndex = i;
-            buttons[i].onClick.AddListener(() => OnExplain(capturedIndex));
-            buttonText[i] = buttons[i].GetComponent<TextMeshProUGUI>();
+            listButtons[capturedIndex].onClick.AddListener(() => OnExplain(capturedIndex));
+            var textMesh = listButtons[capturedIndex].gameObject.GetComponentInChildren<TextMeshProUGUI>();
+            _buttonText.Add(textMesh);
+            if (SurveyActive[1][capturedIndex])
+            {
+                OnActiveUi(1,capturedIndex);
+            }
         }
         for (int j = 0; j < chapterButton.Count; j++)
         {
@@ -37,14 +43,14 @@ public class UIFile : MonoBehaviour
     private void SetButtons(int chapter)
     {
         file.transform.SetAsLastSibling();
-        buttons[chapter].gameObject.transform.SetAsLastSibling();
+        listButtons[chapter].gameObject.transform.SetAsLastSibling();
         //데이터 넣기
     }
     
     public void OnActiveUi(int chapter, int index)
     {
         SurveyActive[chapter][index] = true;
-        buttonText[index].text = explainNames[index];
+        _buttonText[index].text = explainNames[index];
     }
     
     private void OnExplain(int index)
