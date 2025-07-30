0ccc43b63d60896e304bacd1b6a0afd19b989062
A7442 <parkjuna1121@naver.com>
2025-07-30 19:18:36 +0900

    feat : UIInvestigation 수정

---
+++ Assets/02_Scripts/Enemy/Action/common/SearchTargetAction.cs
@@ -0,0 +1,44 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "SearchTarget", story: "[Self] search [Target] with [distance] for [IsTargetDetected]", category: "Action", id: "fd399354ba76caced6fa4c6fa5180d61")]
+public partial class SearchTargetAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+    [SerializeReference] public BlackboardVariable<float> Distance;
+    [SerializeReference] public BlackboardVariable<bool> IsTargetDetected;
+    
+    protected override Status OnStart()
+    { 
+        Vector3 startPosition = Self.Value.transform.parent.position;
+        
+        float selfPosX = Self.Value.transform.position.x;
+        float playerPosX = Target.Value.transform.position.x;
+        float distance = Mathf.Abs(startPosition.x - playerPosX);
+        
+        if (distance<= Distance.Value)
+        {
+            IsTargetDetected.Value = true;
+        }
+        else if(distance > Distance.Value + 10)
+        {
+            IsTargetDetected.Value = false;
+        }
+        else if (selfPosX - playerPosX < 8)
+        {
+            IsTargetDetected.Value = true;
+        }
+        else
+        {
+            IsTargetDetected.Value = false;
+        }
+        
+        return Status.Success;
+    }
+}
+
