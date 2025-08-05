e4b198015fcb5cc941a9d4d9284fce222bb5c613
gain404 <gain17352@gmail.com>
2025-08-06 01:17:34 +0900

    feat : IntroScript 추가

---
+++ Assets/02_Scripts/UI/UIList/Tutorial/UIScripts.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScripts : MonoBehaviour
+{
+    [SerializeField] private GameObject subtitlePanel;
+    [SerializeField] private TMP_Text subtitleText;
+    [SerializeField] private List<string> promptTextData;
+    private Player _player;
+    private float _textDelayTime = 0.05f;
+    private float _sentenceDelayTime = 0.5f;
+
+    private void Awake()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        _player = player.GetComponent<Player>();
+    }
+    
+    private void Start()
+    {
+        StartCoroutine(StartPrompt());
+    }
+
+    private IEnumerator StartPrompt()
+    {
+        _player.PlayerController.playerActions.Disable();
+        subtitlePanel.SetActive(true);
+        foreach (var prompt in promptTextData)
+        {
+            yield return StartCoroutine(PromptText(prompt));
+            yield return new WaitForSeconds(_sentenceDelayTime);
+        }
+        subtitlePanel.SetActive(false);
+        _player.PlayerController.playerActions.Enable();
+    }
+
+    private IEnumerator PromptText(string text)
+    {
+        subtitleText.text = "";
+        foreach (char c in text)
+        {
+            subtitleText.text += c;
+            yield return new WaitForSeconds(_textDelayTime);
+        }
+    }
+    
+}
