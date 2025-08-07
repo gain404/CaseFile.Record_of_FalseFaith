dd8f6acecba33197fa98dd84b4c6239cb23d5f33
songdh001 <155062686+songdh001@users.noreply.github.com>
2025-08-07 15:30:47 +0900

    feat : 씬 별 재생할 BGM 설정 기능 추가(SceneBGMSetter)

---
+++ Assets/02_Scripts/Audio/CptBGM/SceneBGMSetter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SceneBGMSetter : MonoBehaviour
+{
+    [Header("이 씬에서 재생할 BGM")]
+    public AudioClip sceneBGM;
+
+    private void Start()
+    {
+        if (sceneBGM != null)
+        {
+            SoundManager.Instance.SetAndPlayDefaultBGM(sceneBGM);
+        }
+    }
+}
