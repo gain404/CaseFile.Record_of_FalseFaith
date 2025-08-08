4b618692600c675be532f35a26669e2a4f204195
A7442 <parkjuna1121@naver.com>
2025-08-08 17:02:59 +0900

    feat : UIFile 수정

--- Assets/02_Scripts/Enemy/BossRoom/BossStage1/VengefulSpirit.cs
+++ Assets/02_Scripts/Enemy/BossRoom/BossStage1/VengefulSpirit.cs
@@ -11,13 +11,18 @@ public class VengefulSpirit : MonoBehaviour
     [SerializeField] private EnemyController enemyController;
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer spriteRenderer;
-    //[SerializeField] private SfxPlayer sfxPlayer;
+    [Header("Sound")]
+    [SerializeField] private AudioClip meleeAttackClip;
+    [SerializeField] private AudioClip dieClip;
+    [SerializeField] private AudioClip moveClip;
 
     private PoolManager _poolManager;
+    private SoundManager _soundManager;
     private static readonly int IsDie = Animator.StringToHash("IsDie");
     private void Start()
     {
         _poolManager = PoolManager.Instance;
+        _soundManager = SoundManager.Instance;
         meleeAttackCollider2D.enabled = false;
         biteAttackCollider2D.enabled = false;
         enemyController.DieAction += Die;
@@ -53,25 +58,16 @@ public class VengefulSpirit : MonoBehaviour
         spriteRenderer.DOFade(0.9f, 1.3f).OnComplete(() => gameObject.SetActive(false));
     }
     
-    // private void SoundPlay()
-    // {
-    //     if (meleeAttack.isDamaged)
-    //     {
-    //         sfxPlayer.PlaySfx(SfxName.MeleeAttackSuccess);
-    //         meleeAttack.isDamaged = false;
-    //     }
-    //     else
-    //     {
-    //         sfxPlayer.PlaySfx(SfxName.MeleeAttackFailed);
-    //     }
-    //     if (biteAttack.isDamaged)
-    //     {
-    //         sfxPlayer.PlaySfx(SfxName.BiteSuccess);
-    //         biteAttack.isDamaged = false;
-    //     }
-    //     else
-    //     {
-    //         sfxPlayer.PlaySfx(SfxName.BiteFailed);
-    //     }
-    // }
+    private void MeleeAttackSfx()
+    {
+        _soundManager.PlaySFX(meleeAttackClip);
+    }
+    private void DieSfx()
+    {
+        _soundManager.PlaySFX(dieClip);
+    }
+    private void MoveSfx()
+    {
+        _soundManager.PlaySFX(moveClip);
+    }
 }
--- Assets/02_Scripts/Enemy/CommonEnemy/TutorialEnemy.cs
+++ Assets/02_Scripts/Enemy/CommonEnemy/TutorialEnemy.cs
@@ -8,13 +8,19 @@ public class TutorialEnemy : MonoBehaviour
     [SerializeField] private EnemyController enemyController;
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [Header("Sound")]
+    [SerializeField] private AudioClip attackClip;
+    [SerializeField] private AudioClip dieClip;
+    [SerializeField] private AudioClip moveClip;
     
     private static readonly int IsDie = Animator.StringToHash("IsDie");
+    private SoundManager _soundManager;
 
     private void Start()
     {
         meleeAttackCollider2D.enabled = false;
         enemyController.DieAction += Die;
+        _soundManager = SoundManager.Instance;
     }
     private void MeleeAttack()
     {
@@ -28,5 +34,18 @@ public class TutorialEnemy : MonoBehaviour
     {
         animator.SetBool(IsDie,true);
         spriteRenderer.DOFade(0.9f, 1.0f).OnComplete(() => gameObject.SetActive(false));
+    }
+
+    private void AttackSfx()
+    {
+        _soundManager.PlaySFX(attackClip);
+    }
+    private void DieSfx()
+    {
+        _soundManager.PlaySFX(dieClip);
+    }
+    private void MoveSfx()
+    {
+        _soundManager.PlaySFX(moveClip);
     }
 }
