05a191c191d6f7b863445396f8804d61c2a44fc3
gain404 <gain17352@gmail.com>
2025-08-08 20:37:18 +0900

    feat : 브금 추가 및 플레이어 피벗 변경

--- Assets/02_Scripts/Audio/CptBGM/SceneBGMSetter.cs
+++ Assets/02_Scripts/Audio/CptBGM/SceneBGMSetter.cs
@@ -5,11 +5,22 @@ public class SceneBGMSetter : MonoBehaviour
     [Header("이 씬에서 재생할 BGM")]
     public AudioClip sceneBGM;
 
+    [Header("특정 상황에서 재생할 BGM")]
+    public AudioClip triggerBGM;
+    
     private void Start()
     {
         if (sceneBGM != null)
         {
             SoundManager.Instance.SetAndPlayDefaultBGM(sceneBGM);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && triggerBGM != null)
+        {
+            SoundManager.Instance.SetAndPlayDefaultBGM(triggerBGM);
         }
     }
 }
