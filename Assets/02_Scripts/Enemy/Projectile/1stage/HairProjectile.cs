28e2f217eb202d833e4b9761ab0d0fdad061a696
A7442 <parkjuna1121@naver.com>
2025-08-12 08:23:16 +0900

    Update HairProjectile.cs

--- Assets/02_Scripts/Enemy/Projectile/1stage/HairProjectile.cs
+++ Assets/02_Scripts/Enemy/Projectile/1stage/HairProjectile.cs
@@ -9,33 +9,31 @@ public class HairProjectile : MonoBehaviour
     [SerializeField] private HairProjectileContainer hairProjectileContainer;
     
     private Animator _animator;
-    private PoolManager _poolManager;
     private SoundManager _soundManager;
     private readonly int _hairAttack = Animator.StringToHash("HairAttack");
+    private bool _isDamage;
 
     private void Awake()
     {
         _animator = gameObject.GetComponent<Animator>();
-        _poolManager = PoolManager.Instance;
         _soundManager = SoundManager.Instance;
+        _isDamage = false;
     }
 
     private void OnEnable()
     {
-        _animator = gameObject.GetComponent<Animator>();
-        _poolManager = PoolManager.Instance;
-        _soundManager = SoundManager.Instance;
+        _isDamage = false;
         DOVirtual.DelayedCall(0.5f, () => _animator.SetTrigger(_hairAttack));
     }
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (((1 << collision.gameObject.layer) & hitLayerMask) != 0)
+        if(_isDamage) return;
+        if (((1 << collision.gameObject.layer) & hitLayerMask) == 0) return;
+        if (collision.TryGetComponent(out PlayerStat playerStat))
         {
-            if (collision.TryGetComponent(out PlayerStat playerStat))
-            {
-                playerStat.TakeDamage(1);
-            }
+            playerStat.TakeDamage(1);
+            _isDamage = true;
         }
     }
     private void Finish()
